516bb1baaf6a809529fbc8e88b6d6a77a183b4cd
Noor <noorfarooqy@gmail.com>
2019-04-23 07:23:35 +0800

    style and main js

--- public/js/main.js
+++ public/js/main.js
@@ -18,8 +18,8 @@ $(document).ready(function(){
 	});
     $('.givenContent').html($('.givenContent').text());
 	$('#editor').each(function () {
-    var editor = new Jodit(this, {
-    });
+		var editor = new Jodit(this, {
+		});
 	});
 
 	$('.search-icon').click(function(){
@@ -41,25 +41,36 @@ $(document).ready(function(){
 		$('.menuhover').slideUp(1000);
 	});
 
-	$('.insertImage').click(function(e){
-		e.preventDefault();
-		$('.filePathHolder').text('Loading...');
+	$('.GenVideoCode').click(function(){
+		$('#videoCode').text('Loading...');
 		$('.addFile').click();
 	});
 
 	$('.addFile').change(function(){
 		// alert('will insert image');
-		$('.submitAddFile').click();
+		$('#addFileForm').submit();
 	});
 	$('#addFileForm').submit(function(e){
 		e.preventDefault();
 		// alert('form submitted');
 		var formdata = new FormData(this);
+		// formdata.append('addFile', $("#addFileForm input[type='file']")[0].files);
+		// formdata.append('_token', $("#addFileForm input[type='hidden']").val());
+
 		// formdata.append('addFileFile', 'something')
 		// var url  = ($('.fileTargetUrl').val() == undefined ? 'addFile': $('.fileTargetUrl').val());
 		var url = $(this).attr('action');
 		// console.log(JSON.stringify(formdata));
 		ajax_call(url, formdata, 'fileForUrl');
+		console.log(formdata);
+	});
+	$('.formtoUplaod').submit(function(e){
+		e.preventDefault();
+		alert('form ready');
+	});
+
+	$('.newNews').click(function(){
+		$('#newNews').submit();
 	});
 
 });
@@ -78,15 +89,15 @@ function ajax_call(url, data, indicator)
 			// alert('success '+JSON.stringify(data));
 			if(data.success === true)
 			{
-				$('.filePathHolder').removeClass('bg-danger');
-				$('.filePathHolder').addClass('bg-success');
-				$('.filePathHolder').text(data.fileurl);
+				$('#videoCode').removeClass('bg-danger');
+				$('#videoCode').addClass('bg-success');
+				$('#videoCode').text(data.fileurl);
 			}
 			else
 			{
-				$('.filePathHolder').addClass('bg-danger');
-				$('.filePathHolder').removeClass('bg-success');
-				$('.filePathHolder').text('Failed to add file');
+				$('#videoCode').addClass('bg-danger');
+				$('#videoCode').removeClass('bg-success');
+				$('#videoCode').text('Failed to add file');
 			}
 
 			
@@ -101,8 +112,8 @@ function ajax_call(url, data, indicator)
 		   422: function(error){
 		   	// alert(JSON.stringify(error.responseJSON.message));
 		   	// alert(JSON.stringify(error.responseJSON.errors.addFile[0]));
-		   	$('.filePathHolder').addClass('bg-danger');
-		   	$('.filePathHolder').text(error.responseJSON.errors.addFile[0]);
+		   	$('#videoCode').addClass('bg-danger');
+		   	$('#videoCode').text(error.responseJSON.errors.addFile[0]);
 		   }
 		},
 	})
