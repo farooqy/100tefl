957f0e8ec17c8de29372aae48d3913dec5225b4f
Noor <noorfarooqy@gmail.com>
2019-04-23 07:23:56 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,14 +9,11 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         
         <title>Admin | 100TEFL</title>
-        <script src="js/jquery.js"></script>
-        <script type="text/javascript" src="/js/umd/popper.min.js"></script>
-        <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+        
         <link href="/css/fonts_google.css?family=Lato:300,400,700" rel="stylesheet">
         
-        <link rel="stylesheet" href="/css/icon-font.css">
-        <link rel="stylesheet" href="/css/bootstrap.min.css">
-        <link rel="stylesheet" href="/css/style.css" type="text/css">
+        <link rel="stylesheet" href="../css/icon-font.css" type="text/css">
+        <link rel="stylesheet" href="../css/style.css" type="text/css">
         <link rel="shortcut icon" href="https://100tefl.com/images/favicon.ico" sizes="32x32" type="image/png" >
     </head>
     
@@ -24,7 +21,7 @@
         <header class="pages_header">
             <nav class="row sticky__services">
                 <div class="col-1-of-4 header__logo-box">
-                    <a href="../index.html"><img src="../img/logo.png" alt="logo" class="header__logo-img"></a>
+                    <a href="/"><img src="/img/logo.png" alt="logo" class="header__logo-img"></a>
                 </div>
                 
                 <label for="toggle" class="label-black">&#9776;</label>
@@ -32,33 +29,19 @@
                 
                 <div class="col-3-of-4-drop-down menu">
                     <ul class="pages_header__black-nav">
-                        <li><a href="../index.html" onClick="toggleDropdown()">Home</a></li>
-                        <li><a href="../index.html#section_news" onClick="toggleDropdown()">News</a></li>
-                        <li><a href="../pages/services.html" onClick="toggleDropdown()">Services</a></li>
-                        <li><a href="../index.html#section_contact" onClick="toggleDropdown()">Contact</a></li>
-                        <li class="dropdown">
-                          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Profile
-                          </button>
-                          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">{{Auth::user()->name}}</a>
-                            <a class="dropdown-item" href="/logout" id="Logout">Log out</a>
-                          </div>
-                        </li>
+                        <li><a href="/" onClick="toggleDropdown()">Home</a></li>
+                        <li><a href="/#section_news" onClick="toggleDropdown()">News</a></li>
+                        <li><a href="/services" onClick="toggleDropdown()">Services</a></li>
+                        <li><a href="/#section_contact" onClick="toggleDropdown()">Contact</a></li>
+                        <li><a href="/#section_career-partners" onClick="toggleDropdown()">100tefl VIP</a></li>
+                        <li><a href="/about" onClick="toggleDropdown()">About</a></li>
                     </ul>
                 </div>
             </nav>
         </header>
         
         <main>
-            <div class="row">
-                @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                @endif
-            </div>
-            <div class="row u-margin-top-big">
+            <div class="row u-margin-top-big u-margin-bottom-big">
                 <div class="col-1-of-3 admin-box--card">
                     <div class="admin-box">
                         <a href="/home/addpost"><i class="admin-box__icon icon-basic-pin2"></i></a>
@@ -68,28 +51,19 @@
                 </div>
                 <div class="col-1-of-3">
                     <div class="admin-box">
-                        <a href=""><i class="admin-box__icon icon-basic-share"></i></a>
+                        <a href="home/addUser"><i class="admin-box__icon icon-basic-share"></i></a>
                         <h3 class="heading-tertiary u-margin-bottom-small">Add User</h3>
                         <a class="btn--services services-box__text" href="pages/services.html">细节</a>
                     </div>
                 </div>
                 <div class="col-1-of-3">
                     <div class="admin-box">
-                        <a href=""><i class="admin-box__icon icon-basic-info"></i></a>
+                        <a href="/home/feedbacks"><i class="admin-box__icon icon-basic-info"></i></a>
                         <h3 class="heading-tertiary u-margin-bottom-small">View Feedback</h3>
                         <a class="btn--services services-box__text" href="pages/services.html">细节</a>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <form class="lgout" id="logOutForm" method="post" action="{{route('logout')}}">@csrf</form>
-            </div>
         </main>
-        <script type="text/javascript">
-            $('#Logout').click(function(e){
-                e.preventDefault();
-                $('#logOutForm').submit();
-            });
-        </script>
     </body>
 </html>
